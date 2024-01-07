6a579130b842ea73c8b618d6f58c18f7e729955c
Atin Gupta <atingupta@email.com>
2024-01-07 12:56:50 +0530

    -

--- Day-3-4/04-puppet-programming/commands.sh
+++ Day-3-4/04-puppet-programming/commands.sh
@@ -5,10 +5,10 @@
 cd ~
 rm -rf ~/puppet-jan-24
 git clone https://github.com/atingupta2005/puppet-jan-24/
-cd ~/puppet-jan-24/Day-3-4
 ```
 
 ## Run Various Manifests
+cd ~/puppet-jan-24/Day-3-4
 sudo puppet apply 04-puppet-programming/manifests/01-hash.pp
 sudo puppet apply 04-puppet-programming/manifests/02-hash2.pp
 sudo puppet apply 04-puppet-programming/manifests/03-hashes.pp
@@ -34,31 +34,22 @@ sudo su
 cd /etc/puppetlabs/code/environments/production/modules
 
 pdk new module java --skip-interview 
-pdk new module tomcat --skip-interview 
-
+Y
+pdk new module tomcat --skip-interview
+sudo tree /etc/puppetlabs/code/environments/production/modules
+sudo ls /etc/puppetlabs/code/environments/production/modules
 exit
 
 ## Copy code of Java and Tomcat module from GitHub
 sudo mv /etc/puppetlabs/code/environments/production/manifests/site.pp /etc/puppetlabs/code/environments/production/manifests/site.pp.bkp
-cd /tmp
-sudo rm node-def-1.pp
-wget https://raw.githubusercontent.com/atingupta2005/puppet-jan-24/main/Day-3-4/05-puppet-programming/modules/node-def-1.pp
-sudo cp node-def-1.pp /etc/puppetlabs/code/environments/production/manifests/node-def-1.pp
+sudo cp ~/puppet-jan-24/Day-3-4/04-puppet-programming/modules/node-def-1.pp /etc/puppetlabs/code/environments/production/manifests/node-def-1.pp
+sudo cp ~/puppet-jan-24/Day-3-4/04-puppet-programming/modules/java/manifests/init.pp /etc/puppetlabs/code/environments/production/modules/java/manifests/init.pp
+sudo cp ~/puppet-jan-24/Day-3-4/04-puppet-programming/modules/tomcat/files/tomcat.conf /etc/puppetlabs/code/environments/production/modules/tomcat/files/tomcat.conf
 
-sudo rm init.pp
-wget https://raw.githubusercontent.com/atingupta2005/puppet-jan-24/main/Day-3-4/05-puppet-programming/modules/java/manifests/init.pp
-sudo cp init.pp /etc/puppetlabs/code/environments/production/manifests/modules/java/manifests/init.pp
-
-sudo rm tomcat.conf init.pp config.pp install.pp
-wget https://raw.githubusercontent.com/atingupta2005/puppet-jan-24/main/Day-3-4/05-puppet-programming/modules/tomcat/files/tomcat.conf
-wget https://raw.githubusercontent.com/atingupta2005/puppet-jan-24/main/Day-3-4/05-puppet-programming/modules/tomcat/manifests/init.pp
-wget https://raw.githubusercontent.com/atingupta2005/puppet-jan-24/main/Day-3-4/05-puppet-programming/modules/tomcat/manifests/config.pp
-wget https://raw.githubusercontent.com/atingupta2005/puppet-jan-24/main/Day-3-4/05-puppet-programming/modules/tomcat/manifests/install.pp
-
-sudo cp tomcat.conf /etc/puppetlabs/code/environments/production/manifests/modules/tomcat/files/tomcat.conf
-sudo cp init.pp /etc/puppetlabs/code/environments/production/manifests/modules/tomcat/manifests/init.pp
-sudo cp config.pp /etc/puppetlabs/code/environments/production/manifests/modules/java/manifests/config.pp
-sudo cp install.pp /etc/puppetlabs/code/environments/production/manifests/modules/java/manifests/install.pp
+sudo cp ~/puppet-jan-24/Day-3-4/04-puppet-programming/modules/tomcat/manifests/init.pp /etc/puppetlabs/code/environments/production/modules/tomcat/manifests/init.pp
+sudo cp ~/puppet-jan-24/Day-3-4/04-puppet-programming/modules/tomcat/manifests/config.pp /etc/puppetlabs/code/environments/production/modules/tomcat/manifests/config.pp
+sudo cp ~/puppet-jan-24/Day-3-4/04-puppet-programming/modules/tomcat/manifests/install.pp /etc/puppetlabs/code/environments/production/modules/tomcat/manifests/install.pp
 
 sudo tree /etc/puppetlabs/code/environments/production/manifests
+sudo tree /etc/puppetlabs/code/environments/production/modules
 sudo puppet agent -t
