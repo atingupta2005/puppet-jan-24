be2413eab9c91ad1d7d616de67804f2b40d6dea5
Atin Gupta <atingupta@email.com>
2024-01-09 10:44:43 +0530

    -

--- Day-3-4/04-puppet-programming/modules/tomcat/manifests/config.pp
+++ Day-3-4/04-puppet-programming/modules/tomcat/manifests/config.pp
@@ -2,9 +2,6 @@ class tomcat::config {
 
   file { '/etc/tomcat9/server.xml':
     content => epp('tomcat/server.xml.epp', {'connector_port' => '8282', 'connector_address' => '0.0.0.0'}),
-    owner    => 'tomcat', 
-    group    => 'tomcat', 
-    mode     => '0644',
     notify   => Service['tomcat9']
   }
 }
--- Day-3-4/04-puppet-programming/modules/tomcat/manifests/homepage.pp
+++ Day-3-4/04-puppet-programming/modules/tomcat/manifests/homepage.pp
@@ -2,9 +2,6 @@ class tomcat::homepage {
 
   file { '/var/lib/tomcat9/webapps/ROOT/index.html':
     content => epp('tomcat/index.html.epp', {'h1_message' => 'Hi Team, It works after customizations !'}),
-    owner    => 'root', 
-    group    => 'root', 
-    mode     => '0644',
   }
 }
 
