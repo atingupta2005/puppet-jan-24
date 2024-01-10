580bb577d0a8f49776980fea3dd59e12c5a77fc0
Atin Gupta <atingupta@email.com>
2024-01-10 07:21:29 +0530

    -

--- Day-5-6/03-managing-windows/commands.ps1
+++ Day-5-6/03-managing-windows/commands.ps1
@@ -1,5 +1,6 @@
 # Puppet Programming
 ## Clone GIT Repo
+cd ~
 Remove-Item -Recurse -Force ~/puppet-jan-24
 git clone https://github.com/atingupta2005/puppet-jan-24/
 cd ~/puppet-jan-24/Day-5-6/03-managing-windows
