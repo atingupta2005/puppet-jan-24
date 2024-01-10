9a85db0a3b07997b550209fd7c7627185c2c6f9c
Atin Gupta <atingupta@email.com>
2024-01-10 07:23:09 +0530

    -

--- Day-5-6/03-managing-windows/09-scheduled-tasks-1.pp
+++ Day-5-6/03-managing-windows/09-scheduled-tasks-1.pp
@@ -1,11 +1,6 @@
 scheduled_task { 'Disk Cleanup':
-<<<<<<< HEAD
   command   => "$::system32\\WindowsPowerShell\\v1.0\\powershell.exe",
   arguments => '-File "C:\\hello-script.ps1"',
-=======
-  command   => "powershell.exe",
-  arguments => '-File "C:\\Scripts\\Clear-DiskSpace.ps1"',
->>>>>>> bc3bb795d9a1e643390cbbdd23afc0ace6866a4b
   enabled   => 'true',
   trigger   => [{
     schedule   => 'once',
