b25f296c76db473cdf116a4a2b0f2885cb8bc61e
Atin Gupta <atingupta@email.com>
2024-01-11 10:57:12 +0530

    -

--- Day-7-8/commands-nginx.sh
+++ Day-7-8/commands-nginx.sh
@@ -16,14 +16,14 @@ exit
 
 ## Copy code of Java and nginx module from GitHub
 sudo mv /etc/puppetlabs/code/environments/production/manifests/site.pp /etc/puppetlabs/code/environments/production/manifests/site.pp.bkp
-sudo cp ~/puppet-jan-24/Day-7-8/01-puppet-programming/modules/node-def-1.pp /etc/puppetlabs/code/environments/production/manifests/node-def-1.pp
-sudo cp ~/puppet-jan-24/Day-7-8/01-puppet-programming/modules/nginx/templates/*.epp /etc/puppetlabs/code/environments/production/modules/nginx/templates/
+sudo cp ~/puppet-jan-24/Day-7-8/modules/node-def-1.pp /etc/puppetlabs/code/environments/production/manifests/node-def-1.pp
+sudo cp ~/puppet-jan-24/Day-7-8/modules/nginx/templates/*.epp /etc/puppetlabs/code/environments/production/modules/nginx/templates/
 
-sudo cp ~/puppet-jan-24/Day-7-8/01-puppet-programming/modules/nginx/manifests/*.pp /etc/puppetlabs/code/environments/production/modules/nginx/manifests/
+sudo cp ~/puppet-jan-24/Day-7-8/modules/nginx/manifests/*.pp /etc/puppetlabs/code/environments/production/modules/nginx/manifests/
 
-sudo cp ~/puppet-jan-24/Day-7-8/01-puppet-programming/modules/nginx/hiera.yaml /etc/puppetlabs/code/environments/production/modules/nginx/hiera.yaml
+sudo cp ~/puppet-jan-24/Day-7-8/modules/nginx/hiera.yaml /etc/puppetlabs/code/environments/production/modules/nginx/hiera.yaml
 
-sudo cp -R ~/puppet-jan-24/Day-7-8/01-puppet-programming/modules/nginx/data /etc/puppetlabs/code/environments/production/modules/nginx/
+sudo cp -R ~/puppet-jan-24/Day-7-8/modules/nginx/data /etc/puppetlabs/code/environments/production/modules/nginx/
 
 sudo tree /etc/puppetlabs/code/environments/production/manifests
 sudo tree /etc/puppetlabs/code/environments/production/modules/nginx
